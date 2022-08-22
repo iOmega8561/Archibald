5c0d65e59b6c5dd9b3d00816f7aaa4398c7103c3
iOmega8561 <giusepperocco38@gmail.com>
2022-08-22 13:23:52 +0200

    Code optimizations, project rename

--- Archibald.py
+++ Archibald.py
@@ -3,65 +3,77 @@ import utils.configs as configs
 import utils.methods as methods
 import utils.formats as formats
 
-def main():
+def main(logname):
 	#Prepare greeting text
-	prompt = f"{formats.msgStr} Arcibaldo utility for Arch Linux, select a profile:\n"
+	prompt = f"{formats.msgStr} Archibald utility for Arch Linux, select a profile:\n"
 	
 	#Format-add every profile name and target to greetings
 	for i, p in enumerate(configs.profiles):
 		prompt += formats.selStr.format(i + 1, p.name, p.type)
 	prompt += f"{formats.bold}User input:{formats.endc} "
 
-	#Repeat input until a valid one is given
+	#Print prompt text and get user imput, must be integer
 	index = methods.integerget(prompt, f"{formats.warnStr} Input not a number, retry.") - 1
 	while index < 0 or index >= len(configs.profiles):
+
+		#Repeat input until a valid one is given
 		print(f"{formats.warnStr} Not in range, try again.")
 		index = methods.integerget(prompt, f"{formats.warnStr} Input not a number, retry.") - 1
 
+	#When success, save chosen profile inside selection and prompt user his choice again
 	selection = configs.profiles[index]
 	print(f"{formats.msgStr} Selected profile is {selection.name} ({selection.type})")
 	
 	#Try to find user defined driver name inside lspci output
 	print(f"{formats.warnStr} Searching for any known graphics device...")
-	pcidevices = methods.subprocess_easy("lspci")
+	lspci = methods.subprocess_try("lspci")
 	for device in configs.drivers:
+
 		#Repeat match for every user defined driver group
 		match = [f"VGA compatible controller: {device}", f"Display controller: {device}"]
-		if any(x in pcidevices.stdout.rstrip("\n") for x in match):
+		if any(x in lspci.stdout.rstrip("\n") for x in match):
+
 			#If is found, add packages to profile.pkgs
 			print(f"{formats.succStr} Found {device} device!")
 			selection.pkgs += configs.drivers[device]
 
-	#Packages installation
+	#Packages installation via pacman subprocess
 	print(f"{formats.warnStr} Please wait while PacMan installs packages...")
-	methods.pacman_install(selection.pkgs, f"{formats.succStr} Successfully installed all packages!", f"{formats.errStr} PacMan encountered errors, check pacman.log")
+	methods.subprocess_try(["pacman", "-S", "--needed", "--noconfirm"] + selection.pkgs,
+		"pacman.log", 
+		f"{formats.succStr} Successfully installed all packages!", 
+		f"{formats.errStr} PacMan encountered errors, check pacman.log")
 	
 	#Config files creation
 	print(f"{formats.warnStr} Deploying configuration files...")
 	for i, f in enumerate(configs.files):
 		methods.makefile(f.path, f.name, f.text, f"{formats.errStr} Could not open {f.path}{f.name}.")
 	
-	#Arcibaldo runs with root privileges, so files in home will have rw protection
-	methods.subprocess_easy(["chown", "-R", f"{methods.logname()}", f"/home/{methods.logname()}"])
+	#Archibald runs with root privileges, so files in home will have rw protection
+	methods.subprocess_try(["chown", "-R", f"{logname}", f"/home/{logname}"])
 
 	#Setting user groups one by one
 	print(f"{formats.warnStr} Settings user groups...")
 	for i in selection.groups:
-		methods.subprocess_easy(["usermod", "-aG", f"{methods.logname()}"] + i)
+		methods.subprocess_try(["usermod", "-aG", f"{logname}"] + i)
 
 	#Enabling systemd units all at one (systemctl supports it)
 	print(f"{formats.warnStr} Enabling systemd units...")
-	methods.subprocess_easy(["systemctl", "enable"] + selection.units)
+	methods.subprocess_try(["systemctl", "enable"] + selection.units)
 
-if not methods.subprocess_watch("whoami", "root", "compare"):
+if not methods.subprocess_watch("whoami", "root", "all"):
 	#Exit if not executing with sudo
-	print(f"{formats.errStr} Arcibaldo needs {formats.uline}high{formats.endc} privileges.")
-elif methods.subprocess_watch("logname", "root", "compare"):
-	#Exit if logged in as the root use (/home/root does not exist)
-	print(f"{formats.errStr} Executing logged as root is not supported.")
-elif not methods.subprocess_watch(["cat", "/etc/os-release"], "Arch Linux", "find"):
+	print(f"{formats.errStr} Archibald needs {formats.uline}high{formats.endc} privileges.")
+elif not methods.subprocess_watch(["cat", "/etc/os-release"], "Arch Linux", "any"):
 	#Exit if system is not Arch Linux (pacman is used)
 	print(f"{formats.errStr} This is not Arch Linux.")
 else:
+	logname = methods.subprocess_try("logname").stdout.rstrip("\n")
+	
+	if logname == "root":
+		#Exit if logged in as the root use (/home/root does not exist)
+		print(f"{formats.errStr} Executing logged as root is not supported.")
+		quit()
+
 	#Run main logic
-	main()
+	main(logname)
--- utils/methods.py
+++ utils/methods.py
@@ -4,21 +4,23 @@ def logname():
 	proc = subprocess_easy("logname")
 	return proc.stdout.rstrip("\n")
 
-def integerget(prompt, errStr):
+def integerget(prompt, errStr = None):
 	while True:
 		try:
 			i = int(input(prompt))
 			return i
 		except ValueError:
-			print(errStr)
+			if errStr != None:
+				print(errStr)
 
-def makefile(path, name, text, errStr):
+def makefile(path, name, text, errStr = None):
 	try:
 		os.makedirs(path, exist_ok=True)
 		with open(f"{path}/{name}", "w") as file:
 			file.write(text)
 	except IOError or OSError:
-		print(errStr)
+		if errStr != None:
+			print(errStr)
 		quit()
 
 def subprocess_easy(command, filename = None):
@@ -37,21 +39,24 @@ def subprocess_easy(command, filename = None):
 			text = True)
 	return proc
 
+def subprocess_try(command, file = None, succStr = None, errStr = None):
+	try:
+		proc = subprocess_easy(command, file)
+		if succStr != None:
+			print(succStr)
+		return proc
+	except subprocess.CalledProcessError:
+		if errStr != None:
+			print(errStr)
+		quit()
+
 def subprocess_watch(command, match, mode):
-	proc = subprocess_easy(command)
+	proc = subprocess_try(command)
 	clean = proc.stdout.rstrip("\n")
 	
-	if mode == "compare" and all(x in clean for x in match):
+	if mode == "all" and all(x in clean for x in match):
 		return True
-	elif mode == "find" and any(x in clean for x in match):
+	elif mode == "any" and any(x in clean for x in match):
 		return True
-	else:
-		return False
-
-def pacman_install(packages, succStr, errStr = None):
-	try:
-		proc = subprocess_easy(["pacman", "-S", "--needed", "--noconfirm", "--quiet"] + packages, "pacman.log")
-		print(succStr)
-	except subprocess.CalledProcessError:
-		print(errStr)
-		quit()
+	
+	return False
