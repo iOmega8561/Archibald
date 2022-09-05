3da845878e9d47362df405002bf7e689e26e5d6e
iOmega8561 <giusepperocco38@gmail.com>
2022-09-05 12:41:03 +0200

    s

--- profiles/custom.py
+++ profiles/custom.py
@@ -2,35 +2,29 @@ from profiles import pClass, fClass, pAdd
 
 pAdd(pClass(
     
-    name = "Custom",
+    name     = "Custom",
 
-    type = "Desktop",
+    type     = "Desktop",
 
-    drivers = {
+    drivers  = {
         "NVIDIA Corporation": ["dkms", "nvidia-dkms", "nvidia-prime"],
         "Advanced Micro Devices": ["xf86-video-amdgpu", "libva-mesa-driver", "vulkan-radeon", "mesa-vdpau"],
         "Intel Corporation": ["xf86-video-intel", "libva-mesa-driver", "vulkan-intel", "mesa-vdpau"]
     },
 
-    pkgs = ["acpi", "acpid", "acpi_call", "base-devel", "usbutils", "zsh", "zsh-completions", "net-tools", "gnu-netcat", "openssh", "git", "nano", "networkmanager", "bluez", "bluez-utils", "cronie", "htop", "polkit", "cups", "cups-pdf", "splix"] + ["mesa", "mesa-utils", "wayland", "libinput", "libwacom", "xorg-server", "xorg-xinit"] + ["pipewire", "pipewire-alsa", "pipewire-jack", "pipewire-pulse", "gst-plugin-pipewire", "libpulse", "wireplumber"] + ["gnome-shell", "gdm", "nautilus", "gvfs-mtp", "gnome-keyring", "gnome-terminal", "gnome-tweaks", "power-profiles-daemon", "malcontent", "gnome-control-center", "gnome-backgrounds", "gnome-software", "eog", "gnome-calculator", "gedit", "evince", "xdg-user-dirs-gtk", "xdg-desktop-portal-gnome", "xdg-utils", "libqtxdg", "ttf-liberation", "ttf-droid", "noto-fonts-emoji"],
+    pkgs     = ["acpi", "acpid", "acpi_call", "base-devel", "usbutils", "zsh", "zsh-completions", "net-tools", "gnu-netcat", "openssh", "git", "nano", "networkmanager", "bluez", "bluez-utils", "cronie", "htop", "polkit", "cups", "cups-pdf", "splix", "mesa", "mesa-utils", "wayland", "libinput", "libwacom", "xorg-server", "xorg-xinit", "pipewire", "pipewire-alsa", "pipewire-jack", "pipewire-pulse", "gst-plugin-pipewire", "libpulse", "wireplumber", "gnome-shell", "gdm", "nautilus", "gvfs-mtp", "gnome-keyring", "gnome-terminal", "gnome-tweaks", "power-profiles-daemon", "malcontent", "gnome-control-center", "gnome-backgrounds", "gnome-software", "eog", "gnome-calculator", "gedit", "evince", "xdg-user-dirs-gtk", "xdg-desktop-portal-gnome", "xdg-utils", "libqtxdg", "ttf-liberation", "ttf-droid", "noto-fonts-emoji"],
 
-    units = ["acpid", "bluetooth", "NetworkManager", "cronie", "cups", "gdm"],
+    units    = ["acpid", "bluetooth", "NetworkManager", "cronie", "cups", "gdm"],
 
-    shell = "/bin/zsh",
+    shell    = "/bin/zsh",
 
-    aur = True,
+    aur      = True,
 
-    flatpaks = [
-        "com.github.tchx84.Flatseal",
-        "com.github.GradienceTeam.Gradience",
-        "org.gnome.Boxes"
-    ],
+    flatpaks = ["com.github.tchx84.Flatseal", "com.github.GradienceTeam.Gradience", "org.gnome.Boxes"],
 
-    bashcmd = [
-        "sudo rm -f /usr/share/applications/{avahi-discover.desktop,bssh.desktop,bvnc.desktop}"
-    ],
+    bashcmd  = ["sudo rm -f /usr/share/applications/{avahi-discover.desktop,bssh.desktop,bvnc.desktop}"],
 
-    files = [
+    files    = [
         fClass(
             name = "logind.conf",
             path = "/etc/systemd", 
