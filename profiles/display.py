eb498dd82ef73248250c843fcbc1bb7976532eda
iOmega8561 <giusepperocco38@gmail.com>
2022-09-23 23:19:07 +0200

    nvidia-dkms to nvidia

--- profiles/display.py
+++ profiles/display.py
@@ -3,9 +3,9 @@ deps = ["minimal"]
 name = "Display ready"
 
 gfxd = {
-    "NVIDIA Corporation": ["dkms", "nvidia-dkms", "nvidia-prime"],
+    "NVIDIA Corporation": ["nvidia", "nvidia-utils", "nvidia-prime"],
     "Advanced Micro Devices": ["xf86-video-amdgpu", "libva-mesa-driver", "vulkan-radeon", "mesa-vdpau"],
     "Intel Corporation": ["xf86-video-intel", "libva-mesa-driver", "vulkan-intel", "mesa-vdpau"]
 }
 
-pkgs = ["mesa", "mesa-utils", "wayland", "libinput", "libwacom", "xorg-server", "xorg-xinit", "pipewire", "pipewire-alsa", "pipewire-jack", "pipewire-pulse", "gst-plugin-pipewire", "libpulse", "wireplumber"]
+pkgs = ["mesa", "mesa-utils", "wayland", "libinput", "xorg-server", "xorg-xinit", "pipewire", "pipewire-alsa", "pipewire-jack", "pipewire-pulse", "gst-plugin-pipewire", "libpulse", "wireplumber"]
