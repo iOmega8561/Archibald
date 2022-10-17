c2731e2799bd252c02b83bd4ad1ee5c1c7e9754a
iOmega8561 <giusepperocco38@gmail.com>
2022-10-17 21:47:50 +0200

    capocchia

--- profiles/cockpit.py
+++ profiles/cockpit.py
@@ -2,8 +2,8 @@ deps   = ["minimal"]
 
 name   = "Cockpit server"
 
-pkgs   = ["screen", "cockpit", "udisks2", "cockpit-storaged", "dmidecode", "dnsmasq", "qemu-base", "libvirt", "virt-install", "cockpit-machines", "docker"]
+pkgs   = ["screen", "cockpit", "firewalld", "udisks2", "cockpit-storaged", "dmidecode", "dnsmasq", "qemu-base", "libvirt", "virt-install", "cockpit-machines", "cockpit-podman", "podman", "podman-docker"]
 
-units  = ["sshd", "libvirtd", "cockpit.socket", "docker"]
+units  = ["sshd", "libvirtd", "cockpit.socket", "podman"]
 
-groups = ["qemu", "libvirt", "docker"]
+groups = ["qemu", "libvirt"]
