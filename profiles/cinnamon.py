e4b563dc352ceda33ddc043d6267b1e190adcf8f
iOmega8561 <giusepperocco38@gmail.com>
2023-04-05 22:34:23 +0200

    Added new Cinnamon profile

---
+++ profiles/cinnamon.py
@@ -0,0 +1,7 @@
+deps  = ["display"]
+
+name  = "Cinnamon"
+
+pkgs  = ["lightdm", "lightdm-gtk-greeter", "cinnamon", "gnome-terminal", "gpicview", "xdg-desktop-portal-gtk", "xdg-user-dirs-gtk", "xdg-utils"]
+
+units = ["lightdm"]
