b329c0565c256c32b4eacd3f2cbb0326daa6b77f
iOmega8561 <giusepperocco38@gmail.com>
2023-04-06 11:12:00 +0200

    Added translations package to cinnamon profile

--- profiles/cinnamon.py
+++ profiles/cinnamon.py
@@ -2,6 +2,6 @@ deps  = ["display"]
 
 name  = "Cinnamon"
 
-pkgs  = ["lightdm", "lightdm-gtk-greeter", "cinnamon", "gnome-terminal", "gpicview", "xdg-desktop-portal-gtk", "xdg-user-dirs-gtk", "xdg-utils"]
+pkgs  = ["lightdm", "lightdm-gtk-greeter", "cinnamon", "cinnamon-translations", "gnome-terminal", "gpicview", "xdg-desktop-portal-gtk", "xdg-user-dirs-gtk", "xdg-utils"]
 
 units = ["lightdm"]
