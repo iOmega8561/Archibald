501dd5d660eadf8d73cef3e9f09250cc196df7fd
Giuseppe Rocco <111569153+iOmega8561@users.noreply.github.com>
2023-04-11 20:49:59 +0200

    Update plasma.py
    Added print manager to plasma profile

--- profiles/plasma.py
+++ profiles/plasma.py
@@ -2,6 +2,6 @@ deps  = ["display"]
 
 name  = "Plasma desktop"
   
-pkgs  = ["plasma-desktop", "sddm", "sddm-kcm", "plasma-wayland-session", "dolphin", "systemsettings", "plasma-pa", "kde-gtk-config", "kscreen", "breeze-gtk", "kwallet", "konsole", "bluedevil", "power-profiles-daemon", "powerdevil", "plasma-nm", "plasma-workspace-wallpapers", "discover", "ark", "kate", "kcalc", "spectacle", "krunner", "plasma-disks", "partitionmanager", "okular", "xdg-desktop-portal-kde", "xdg-desktop-portal-gnome", "xdg-utils", "libqtxdg"]
+pkgs  = ["plasma-desktop", "sddm", "sddm-kcm", "plasma-wayland-session", "dolphin", "systemsettings", "plasma-pa", "kde-gtk-config", "kscreen", "breeze-gtk", "kwallet", "konsole", "bluedevil", "power-profiles-daemon", "powerdevil", "plasma-nm", "plasma-workspace-wallpapers", "discover", "ark", "kate", "kcalc", "spectacle", "krunner", "plasma-disks", "partitionmanager", "okular", "xdg-desktop-portal-kde", "xdg-desktop-portal-gnome", "xdg-utils", "libqtxdg", "print-manager"]
 
 units = ["sddm"]
