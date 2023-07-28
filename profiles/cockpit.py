905efcdcfca1c8a4876855560d2209fba05e2870
iOmega8561 <giusepperocco38@gmail.com>
2023-07-28 09:11:03 +0000

    cockpit-pcp added to cockpit profile

--- profiles/cockpit.py
+++ profiles/cockpit.py
@@ -2,7 +2,7 @@ deps   = ["minimal"]
 
 name   = "Cockpit server"
 
-pkgs   = ["screen", "cockpit", "firewalld", "udisks2", "cockpit-storaged", "dmidecode", "dnsmasq", "qemu-base", "libvirt", "virt-install", "cockpit-machines", "cockpit-podman", "podman", "podman-docker"]
+pkgs   = ["screen", "cockpit", "firewalld", "udisks2", "cockpit-storaged", "dmidecode", "dnsmasq", "qemu-base", "libvirt", "virt-install", "cockpit-machines", "cockpit-podman", "podman", "podman-docker", "cockpit-pcp"]
 
 units  = ["sshd", "libvirtd", "cockpit.socket"]
 
