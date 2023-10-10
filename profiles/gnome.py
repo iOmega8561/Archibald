7ff02190d972f513062e852a2e2dd90a1201f4e5
iOmega8561 <giusepperocco38@gmail.com>
2023-10-10 21:20:25 +0200

    Replaced eog with loupe for gnome 45

--- profiles/gnome.py
+++ profiles/gnome.py
@@ -2,6 +2,6 @@ deps  = ["display"]
 
 name  = "Gnome desktop"
 
-pkgs  = ["gnome-shell", "gdm", "nautilus", "gvfs-mtp", "gnome-keyring", "gnome-console", "gnome-tweaks", "power-profiles-daemon", "malcontent", "gnome-control-center", "gnome-backgrounds", "gnome-software", "eog", "gnome-calculator", "gnome-text-editor", "evince", "xdg-user-dirs-gtk", "xdg-desktop-portal-gnome", "xdg-utils", "libqtxdg"]
+pkgs  = ["gnome-shell", "gdm", "nautilus", "gvfs-mtp", "gnome-keyring", "gnome-console", "gnome-tweaks", "power-profiles-daemon", "malcontent", "gnome-control-center", "gnome-backgrounds", "gnome-software", "loupe", "gnome-calculator", "gnome-text-editor", "evince", "xdg-user-dirs-gtk", "xdg-desktop-portal-gnome", "xdg-utils", "libqtxdg"]
 
 units = ["gdm"]
