3e6e7fac57099ea087555829bf00ffe2578e01dc
iOmega8561 <giusepperocco38@gmail.com>
2023-12-17 11:44:22 +0100

    nvidia-utils explicited, removed ancient intel driver

--- profiles/display.py
+++ profiles/display.py
@@ -3,9 +3,9 @@ deps = ["minimal"]
 name = "Display ready"
 
 gfxd = {
-    "NVIDIA Corporation": ["nvidia-dkms"], #laptops should also install nvidia-prime
+    "NVIDIA Corporation": ["nvidia-dkms", "nvidia-utils"],
     "Advanced Micro Devices": ["xf86-video-amdgpu", "libva-mesa-driver", "vulkan-radeon", "mesa-vdpau"],
-    "Intel Corporation": ["xf86-video-intel", "libva-mesa-driver", "vulkan-intel", "mesa-vdpau"]
+    "Intel Corporation": ["libva-mesa-driver", "vulkan-intel", "mesa-vdpau"]
 }
 
 pkgs = ["mesa", "mesa-utils", "wayland", "libinput", "xorg-server", "xorg-xinit", "pipewire", "pipewire-alsa", "pipewire-jack", "pipewire-pulse", "gst-plugin-pipewire", "libpulse", "wireplumber"]
